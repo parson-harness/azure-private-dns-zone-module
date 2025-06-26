acec86cd8d5eb524c08d9d559e3ee5682e13b8e2
parson-harness <todd.parson@harness.io>
2025-06-26 18:10:54 -0500

    Adding output for module_version in-use

--- modules/azure-private-dns-zone-module/outputs.tf
+++ modules/azure-private-dns-zone-module/outputs.tf
@@ -7,3 +7,7 @@ output "dns_zone_name" {
   value       = azurerm_private_dns_zone.this.name
   description = "The name of the Private DNS Zone"
 }
+output "module_version" {
+  value       = "v1.0.4"
+  description = "The version of the module in use"
+}
